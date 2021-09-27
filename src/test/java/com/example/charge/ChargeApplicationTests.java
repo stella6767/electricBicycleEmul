b2d2a261fe282869484c86bd70affd1e123bed2c
강민규 <alsrb9434@gmail.com>
2021-09-27 13:50:01 +0900

    charger 완

--- src/test/java/com/example/charge/ChargeApplicationTests.java
+++ src/test/java/com/example/charge/ChargeApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.charge;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ChargeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class ChargeApplicationTests {
+//
+////    @Test
+////    void contextLoads() {
+////    }
+//
+//}
